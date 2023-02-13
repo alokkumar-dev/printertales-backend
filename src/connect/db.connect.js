fdd1972bf0489d287df1b32e070d0a4f58fe3a85
alokkumar-dev <alokshakya2601@gmail.com>
2023-02-13 21:03:40 +0530

    change atlus key and change data

--- src/connect/db.connect.js
+++ src/connect/db.connect.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
+
 
 const connect = () => {
   return mongoose.connect(
-    "mongodb+srv://egss:egss@cluster0.oqe184v.mongodb.net/printertalesdb?retryWrites=true&w=majority"
+    "mongodb+srv://alok:alok@cluster0.fmya8.mongodb.net/printertalesdb?retryWrites=true&w=majority"
   );
 };
 module.exports = connect;
